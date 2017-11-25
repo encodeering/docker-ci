645989ff054ae6c37cc361113d277385949e306b
Michael Clausen <encodeering@gmail.com>
2017-11-25 19:07:57 +0100

    updates install script version
    * latest version
    
    NOTE
    
    * could be an option to make the version configurable

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # author: Michael Clausen <encodeering@gmail.com>
 
-VERSION=0.1.0
+VERSION=0.1.1
 
 wget -q -O - "https://github.com/encodeering/docker-ci/releases/download/${VERSION}/ci-${VERSION}.tar" | tar xvf - --strip-components=1

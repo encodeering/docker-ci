8d602f55bbba830a7116a5da9223f1fc9a5a053e
Michael Clausen <encodeering@gmail.com>
2017-11-27 11:23:59 +0100

    updates install script to use either a pinned or latest version
    * latest should be used by nighty unstable branch, pinned for production

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # author: Michael Clausen <encodeering@gmail.com>
 
-VERSION=0.1.1
+latest () {
+    echo `curl --silent https://api.github.com/repos/encodeering/docker-ci/releases/latest | awk '/tag_name/ { print $2 }' | cut -d '"' -f 2`
+}
+
+VERSION=${1:-`latest`}
 
 wget -q -O - "https://github.com/encodeering/docker-ci/releases/download/${VERSION}/ci-${VERSION}.tar" | tar xvf - --strip-components=1

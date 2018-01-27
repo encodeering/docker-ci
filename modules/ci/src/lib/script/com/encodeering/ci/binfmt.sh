3ddf9a89b90f1586109822b1f684339af1fd0bbc
Michael Clausen <encodeering@gmail.com>
2018-01-27 18:22:15 +0100

    loads binftm translation only if architecture differs
    * it shouldn't be an requirement to load binfmt definitions if we build an image on real hardware
    
    NOTE
    
    * binfmt requires sudo

--- modules/ci/src/lib/script/com/encodeering/ci/binfmt.sh
+++ modules/ci/src/lib/script/com/encodeering/ci/binfmt.sh
@@ -17,6 +17,8 @@ done
 emulate () {
     local architecture="$1";
 
+    [    "${architecture}" == `dpkg --print-architecture` ] && exit 0
+
     case "${architecture}" in
         armhf )
             local ARM=':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:'

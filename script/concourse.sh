bd91fec8f0a7c08795fb37f9d628a04ffb5465f0
Michael Clausen <encodeering@gmail.com>
2022-03-07 19:12:31 +0100

    configures git branch in the docker pipeline
    * might become handy for testing pipeline code that is not yet tested

--- script/concourse.sh
+++ script/concourse.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# bash <(curl -fsSL https://raw.githubusercontent.com/encodeering/docker-ci/master/script/concourse.sh) pipeline developer encodeering docker-base https://github.com/encodeering/docker-base.git
+# bash <(curl -fsSL https://raw.githubusercontent.com/encodeering/docker-ci/master/script/concourse.sh) pipeline developer encodeering docker-base https://github.com/encodeering/docker-base.git unstable?
 set -euo pipefail
 
 pipeline () {
@@ -7,16 +7,18 @@ pipeline () {
     local repository="${2}"
     local name="${3}"
     local location="${4}"
+    local branch="${5}"
 
     curl -sSL https://raw.githubusercontent.com/encodeering/docker-ci/master/modules/pipeline/docker-self.yml \
         | fly -t "${team}" set-pipeline --pipeline     "${name}-self"                                         \
                                         --var pipeline="${name}"                                              \
                                         --var repository="${repository}"                                      \
                                         --var git.uri="${location}"                                           \
+                                        --var git.branch="${branch}"                                          \
                                         --config -
 }
 
 case "${1}" in
-  pipeline) pipeline "${2}" "${3}" "${4}" "${5}";;
+  pipeline) pipeline "${2}" "${3}" "${4}" "${5}" "${6:-}";;
          *) false;;
 esac || exit 1
